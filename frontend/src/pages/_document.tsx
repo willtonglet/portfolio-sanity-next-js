7daab6472d84ae8f591ae74c444ce4c8f2cc35e7
William Tonglet <william.tonglet@wipro.com>
2020-11-30 09:47:40 -0300

    corrige breakpoint

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -6,10 +6,6 @@ class CustomDocument extends Document {
             <Html lang="pt-br">
                 <Head>
                     <link rel="icon" type="image/x-icon" href="./logo.svg" />
-                    <link
-                        href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
-                        rel="stylesheet"></link>
-                    <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
                 </Head>
                 <body>
                     <Main />
